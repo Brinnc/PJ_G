7444eb8e77e374b5fa0084cf611de5bc24e255a7
Brinc <serapiny@gmail.com>
2023-06-13 18:25:32 +0900

    하트처리

--- HP.js
+++ HP.js
@@ -1,3 +1,18 @@
 class HP extends GameObject{
-    
+    constructor(container, src, width, height, x, y, velX, velY){
+        super(container, src, width, height, x, y, velX, velY);
+
+        this.hasHeart=true;
+
+    }    
+
+    setImage(flag){
+        this.hasHeart=flag;
+        
+        if(flag){
+            this.img.src="./images/heart.png";
+        }else{
+            this.img.src="./images/death.png";
+        }
+    }
 }
